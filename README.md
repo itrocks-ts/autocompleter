712d4a58be898f460d15addb693b9e6c73992c34
Baptiste Pillot <baptiste@pillot.fr>
2025-01-24 11:31:51 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![npm version](https://img.shields.io/npm/v/@itrocks/autocompleter?logo=npm)](https://www.npmjs.org/package/@itrocks/autocompleter)
+[![npm downloads](https://img.shields.io/npm/dm/@itrocks/autocompleter)](https://www.npmjs.org/package/@itrocks/autocompleter)
+[![GitHub](https://img.shields.io/github/last-commit/itrocks-ts/autocompleter?color=2dba4e&label=commit&logo=github)](https://github.com/itrocks-ts/autocompleter)
+[![issues](https://img.shields.io/github/issues/itrocks-ts/autocompleter)](https://github.com/itrocks-ts/autocompleter/issues)
+[![discord](https://img.shields.io/discord/1314141024020467782?color=7289da&label=discord&logo=discord&logoColor=white)](https://25.re/ditr)
+
+# autocompleter
---
+++ autocompleter.ts
@@ -0,0 +1,92 @@
+import autocomplete      from '../../autocompleter/autocomplete.es.js'
+import { PreventSubmit } from '../../autocompleter/autocomplete.es.js'
+import { loadCss }       from '../asset-loader/asset-loader.js'
+
+type Item = {
+	label: string,
+	value: number
+}
+
+export default function autoCompleter(input: HTMLInputElement)
+{
+	loadCss('/node_modules/autocompleter/autocomplete.css')
+	loadCss('/node_modules/@itrocks/autocompleter/autocompleter.css')
+
+	autocomplete({
+		input,
+
+		fetch: (text, update) =>
+		{
+			const dataFetch = input.dataset.fetch ?? input.closest<HTMLElement>('[data-fetch]')?.dataset.fetch
+			const requestInit: RequestInit = { headers: { Accept: 'application/json' } }
+			const summaryRoute = dataFetch + '?startsWith=' + text
+			fetch(summaryRoute, requestInit).then(response => response.text()).then((json) => {
+				const summary: [string, string][] = JSON.parse(json)
+				const startsWith  = input.value.toLowerCase()
+				const suggestions = summary.map(([id, summary]) => ({ label: summary, value: +id }))
+					.filter(item => item.label.toLowerCase().startsWith(startsWith))
+				update(suggestions)
+			})
+		},
+
+		minLength: 0,
+
+		onSelect: (item: Item, input: HTMLInputElement | HTMLTextAreaElement) =>
+		{
+			if (item.value) {
+				if (input.nextElementSibling instanceof HTMLInputElement) {
+					input.nextElementSibling.value = item.value + ''
+				}
+				else {
+					let dotPosition = input.id.lastIndexOf('.')
+					const name = (dotPosition < 0) ? input.id : input.id.slice(0, dotPosition)
+					input.id = input.name = name + '.' + item.value
+				}
+			}
+			if (item.label) {
+				input.value = item.label
+				input.dispatchEvent(new Event('input', { cancelable: true }))
+			}
+		},
+
+		preventSubmit: PreventSubmit.OnSelect
+	})
+
+	input.addEventListener('change', () => {
+		if (input.value.length) return
+
+		if (input.nextElementSibling instanceof HTMLInputElement) {
+			input.nextElementSibling.value = ''
+			return
+		}
+
+		let dotPosition = input.id.lastIndexOf('.')
+		const name = (dotPosition < 0) ? input.id : input.id.slice(0, dotPosition)
+		input.id = input.name = name
+
+		const parent = input.parentElement
+		const item   = (parent instanceof HTMLLIElement) ? parent : input
+		if (!item.parentElement || (item === item.parentElement.lastElementChild)) return
+		item.remove()
+	})
+
+	if (!input.dataset.fetch) {
+		const parent = input.parentElement
+		const item   = (parent instanceof HTMLLIElement) ? parent : input
+
+		input.addEventListener('input', () => {
+			if (item.nextElementSibling || !input.value.length || !item.parentElement) return
+
+			const newItem  = item.cloneNode(true) as HTMLInputElement | HTMLLIElement
+			const newInput = (newItem instanceof HTMLLIElement) ? newItem.getElementsByTagName('input')[0] : newItem
+			newInput.value = ''
+			item.parentElement.append(newItem)
+			setTimeout(() => { newInput.dispatchEvent(new Event('input', { cancelable: true })) })
+		})
+	}
+}
+
+export function multiple(list: HTMLUListElement)
+{
+	list.querySelectorAll<HTMLInputElement>('input').forEach(autoCompleter)
+}
